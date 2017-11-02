d2d811978c072bca7d8a7bf9aca32f67b72967da
bel_m <maxime.bel@epitech.eu>
2017-11-02 11:09:23 +0100

    gonna pull

---
+++ gainCalculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+import os
+
+class gainCalculator():
+
+    currentCycleGain = 0
+
+
+    def getCloudCloud():
+        pass
+
+    def calcRI():
+        pass
+
+    def calcIOPS():
+        pass
+
+    def calcStopStart():
+        pass
